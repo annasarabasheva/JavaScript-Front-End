d9303a372dd8f1a73bacb4743511773f59f29a6b
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-05-07 17:09:15 +0300

    Delete DOM Introduction directory

--- DOM Introduction/02.Collect List Items/CollectListItems.js
+++
@@ -1,7 +0,0 @@
-function extractText() {
-    let listElements = document.getElementsByTagName('li');
-    let textElemnt = document.getElementById('result');
-    for (let element of listElements) {
-        textElemnt.value += element.textContent + "\n";
-    }
-}
--- DOM Introduction/04.Show More/ShowMore.js
+++
@@ -1,6 +0,0 @@
-function showText() {
-    let newElement = document.getElementById('text');
-    newElement.style.display = 'inline';
-    const readMore = document.getElementById('more');
-    readMore.style.display = 'none';
-}
