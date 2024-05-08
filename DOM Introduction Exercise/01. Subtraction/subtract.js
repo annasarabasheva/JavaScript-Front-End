d3d6e5d2b8e88a10118f0bcf56ca0d45ce0bd632
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-05-08 12:35:56 +0300

    Delete DOM Introduction Exercise directory

--- DOM Introduction Exercise/01. Subtraction/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    let firstElement = document.getElementById('firstNumber');
-    let secondElement = document.getElementById('secondNumber');
-    let resultElement = document.getElementById('result');
-    let result = Number(firstElement.value) - Number(secondElement.value)
-    resultElement.textContent = result
-    console.log(resultElement.textContent);
-}
