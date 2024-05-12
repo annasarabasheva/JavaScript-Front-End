5130f1527d0badb29a6c8c8c119619bd26cc3b21
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-05-12 23:44:05 +0300

    Delete JS Objects and Classes directory

--- JS Objects and Classes/convertToJSON.js
+++
@@ -1,7 +0,0 @@
-function solve(firstName, lastName, hairColor) {
-    let object = {name: firstName, lastName: lastName, hairColor: hairColor}
-    let JSONFromObject = JSON.stringify(object)
-    console.log(JSONFromObject);
-}
-
-solve('George', 'Jones', 'Brown')
--- JS Objects and Classes/convertToObject.js
+++
@@ -1,7 +0,0 @@
-function solve(json) {
-    let objectFromJSON = JSON.parse(json);
-    Object.entries(objectFromJSON).forEach(line => console.log(`${line[0]}: ${line[1]}`));
-}
-
-
-solve('{"name": "George", "age": 40, "town": "Sofia"}');
