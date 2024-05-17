c7a1165f293f3627a54bc36d65b618600c941bf1
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-05-17 13:47:33 +0300

    Delete JS Arrays and Strings Exercise directory

--- JS Arrays and Strings Exercise/01.js
+++
@@ -1,8 +0,0 @@
-function solve(array, rotations) {
-    for (let i = 0; i < rotations; i++) {
-        num = array.shift()
-        array.push(num)
-    }
-    console.log(array.join(' '));
-}
-solve([2, 4, 15, 31], 5);
--- JS Arrays and Strings Exercise/08.js
+++
@@ -1,6 +0,0 @@
-function solve(string) {
-    const words = string.split(/(?=[A-Z])/);
-    console.log(words.join(', '));
-    
-}
-solve('SplitMeIfYouCanHaHaYouCantOrYouCan')
