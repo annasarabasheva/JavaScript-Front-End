9341d4e857cc7bb7d6cfd9c9b36f5ce898a23f30
Anna Sarabasheva <162803681+annasarabasheva@users.noreply.github.com>
2024-05-19 21:18:03 +0300

    Delete JS Arrays and Strings directory

--- JS Arrays and Strings/02.js
+++
@@ -1,9 +0,0 @@
-function solve(num, elements) {
-    let numbers = [];
-    for (let i = 0; i < num; i++) {
-        numbers.push(elements[i])
-    }
-    console.log(numbers.reverse().join(' '));
-}
-
-solve(3, [10, 20, 30, 40, 50]);
